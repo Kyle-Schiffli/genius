1042ac03b13ddf1f618917cd21c4e2429f8b7697
Kyle-Schiffli <Kyle.Schiffli@gmail.com>
2020-01-14 21:12:36 -0500

    set up

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
 
 
 client_id = "o6cufaoDMCc52BXz5Ffwf3TAlYZwsojg3oSA04_6jNOYaS7tlltN2GmEuHv9E0zz"
 client_secret = "dj4nMCMImvgq1NGGrkkF-TBF55yUROyoPylyOBKVGZN0vdidGYraQypyWVX-5Df6_w49DiWwqOddD7Y9YXtqhg"
 access_token = "oZ9Oq3vmbyim-CU8MtDA6YacFAOcyfyGKTR1V0HUI4vxE8PAV7pPF5KPhNVEPmt7"
+
+
+app.get('/', function (req, res) {
+  res.send('test 3');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
