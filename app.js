84151f29c8646cfb832a0af668032f1138e591c9
Kyle-Schiffli <Kyle.Schiffli@gmail.com>
2020-01-15 16:32:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,24 +3,22 @@ var app = express();
 const fetch = require('node-fetch');
 
 
-client_id = "o6cufaoDMCc52BXz5Ffwf3TAlYZwsojg3oSA04_6jNOYaS7tlltN2GmEuHv9E0zz"
-client_secret = "dj4nMCMImvgq1NGGrkkF-TBF55yUROyoPylyOBKVGZN0vdidGYraQypyWVX-5Df6_w49DiWwqOddD7Y9YXtqhg"
-access_token = "oZ9Oq3vmbyim-CU8MtDA6YacFAOcyfyGKTR1V0HUI4vxE8PAV7pPF5KPhNVEPmt7"
+var client_id = "o6cufaoDMCc52BXz5Ffwf3TAlYZwsojg3oSA04_6jNOYaS7tlltN2GmEuHv9E0zz"
+var client_secret = "dj4nMCMImvgq1NGGrkkF-TBF55yUROyoPylyOBKVGZN0vdidGYraQypyWVX-5Df6_w49DiWwqOddD7Y9YXtqhg"
+var access_token = "oZ9Oq3vmbyim-CU8MtDA6YacFAOcyfyGKTR1V0HUI4vxE8PAV7pPF5KPhNVEPmt7"
+var song_title = "";
+var artist_name = "";
+var url ='https://api.genius.com/search';
 
 app.get('/', function (req, res) {
-  var url ='https://api.genius.com;
+
   var headers = {
-    "Content-Type": "application/json",
-    "client_id": client_id,
-    "client_secret": client_secret,
-    "credentials": access_token
+    'Authorization': 'Bearer ' + access_token
   }
   var data = {
-    "name": "Wade Wilson",
-    "occupation": "Murderer",
-    "age": "30 (forever)"
+    'q': song_title + ' ' + artist_name
   }
-  fetch(url, { method: 'GET', headers: headers, body: data})
+  fetch(url, { method: 'GET', headers: headers, params: data})
     .then((res) => {
        return res.json()
   })
@@ -34,4 +32,3 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
